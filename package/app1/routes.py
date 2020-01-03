0d6074d4b8d9f8b41bc6acb4f8f28c670f114c75
jakestaab <53784061+jakestaab@users.noreply.github.com>
2020-01-02 19:24:34 -0600

    Create routes.py

---
+++ package/app1/routes.py
@@ -0,0 +1,24 @@
+from flask import render_template, url_for, redirect
+from app1 import app
+from app1.forms import RegistrationForm, LoginForm
+from app1.models import User, Post
+
+
+@app.route("/")
+@app.route("/index")
+def home():
+	return render_template('index.html')
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+	form = RegistrationForm()
+	if form.validate_on_submit():
+		return redirect(url_for('home'))
+	return render_template('register.html', form=form)
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+	form = LoginForm()
+	if form.validate_on_submit():
+		return redirect(url_for('home'))
+	return render_template('login.html', form=form)
